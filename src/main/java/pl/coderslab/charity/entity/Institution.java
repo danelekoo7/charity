06e0f7fbcc7062abe35ba4b47b48e7035c04d58d
danelekoo7 <danieljedrus@gmail.com>
2020-07-21 12:58:07 +0200

    * created institution entity * created institution repository

---
+++ src/main/java/pl/coderslab/charity/entity/Institution.java
@@ -0,0 +1,20 @@
+package pl.coderslab.charity.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Institution {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    String description;
+}
