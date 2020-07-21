15df59c20d80bc017b2782af09c7edd6d6dd4c6f
danelekoo7 <danieljedrus@gmail.com>
2020-07-21 17:14:30 +0200

    * displaying sumAllQuantity from DB

---
+++ src/main/java/pl/coderslab/charity/repository/DonationRepository.java
@@ -0,0 +1,14 @@
+package pl.coderslab.charity.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import pl.coderslab.charity.entity.Category;
+import pl.coderslab.charity.entity.Donation;
+
+import java.util.List;
+
+public interface DonationRepository extends JpaRepository<Donation, Long> {
+    @Query(value = "SELECT SUM(quantity) FROM donation", nativeQuery = true)
+    Integer sumAllQuantity();
+}
